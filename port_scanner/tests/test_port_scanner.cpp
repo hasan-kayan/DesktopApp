ce72871752a73005e8df6c1b90aba35fddfc3e25
hasan-kayan <hasankayan2000@hotmail.com>
2024-09-16 14:51:45 +0300

    Created os log

--- port_scanner/tests/test_port_scanner.cpp
+++ port_scanner/tests/test_port_scanner.cpp
@@ -2,6 +2,17 @@
 #include "port_scanner.h"
 
 int main() {
+    // Detect and print the operating system
+    #ifdef _WIN32
+        std::cout << "Operating System: Windows\n";
+    #elif defined(__APPLE__)
+        std::cout << "Operating System: macOS\n";
+    #elif defined(__linux__)
+        std::cout << "Operating System: Linux\n";
+    #else
+        std::cout << "Operating System: Unknown\n";
+    #endif
+
     // Scan all ports (USB and UART)
     std::vector<PortInfo> ports = PortScanner::scanAllPorts();
 
