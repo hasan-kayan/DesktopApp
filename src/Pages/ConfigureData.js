b9c0fcd17f2507d565d7b238961f377a8ab1aad2
hasan-kayan <hasankayan2000@hotmail.com>
2024-09-16 13:29:47 +0300

    Clear

--- src/Pages/ConfigureData.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ConfigureData() {
-  return <h1>Configure Your Data</h1>;
-}
-
-export default ConfigureData;
--- src/Pages/ManageGraphs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ManageGraphs() {
-  return <h1>Manage Your Graphs</h1>;
-}
-
-export default ManageGraphs;
